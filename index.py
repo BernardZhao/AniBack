8ec10548965adc1283849b72cbebdd718e57b0d2
Bernard Zhao <bernard.zhao.us@gmail.com>
2018-09-03 01:08:12 -0700

    architecture changes

--- index.py
+++ index.py
@@ -2,42 +2,47 @@ import json
 import datetime
 import boto3
 
+client = boto3.client('dynamodb')
+
+routes = {
+  "/": root,
+  "/sketches": sketches
+}
+
+def root(action, params):
+  return {
+    "validEndpoints": routes.keys()
+  }
+
+def sketches(action, params):
+  if action == "GET":
+    if params:
+      record = client.get_item(
+        TableName='Sketches',
+        Key={'sketchId':{'S':params["sketchId"]}}
+      )
+      return {
+        'description': record['Item']['description']['S'],
+        'sketchId': record['Item']['sketchId']['S'],
+        'title': record['Item']['title']['S'],
+      }
+    else:
+      paginator = client.get_paginator('scan')
+      response = {
+        "items": []
+      }
+      for page in paginator.paginate(TableName='Sketches'):
+        for record in page["Items"]:
+          response["items"].append(record)
+      return response
+
 def handler(event, context):
   try:
-    requestType = event["httpMethod"]
-    path = event["path"]
-    client = boto3.client('dynamodb')
-
-    response = {}
-
-    if path == "/":
-      response = {
-        "validEndpoints":["/sketches"]
-      }
-  
-    elif path == "/sketches":
-        if requestType == "GET":
-
-          if event["queryStringParameters"]:
-            record = client.get_item(TableName='Sketches', Key={'sketchId':{'S':event["queryStringParameters"]["sketchId"]}})
-            response = {
-              'description': record['Item']['description']['S'],
-              'sketchId': record['Item']['sketchId']['S'],
-              'title': record['Item']['title']['S'],
-            }
-
-          else:
-            paginator = client.get_paginator('scan')
-            response["items"] = []
-            for page in paginator.paginate(TableName='Sketches'):
-              for record in page["Items"]:
-                response["items"].append(record)
-
-
-  
+    response = routes.get(event["path"], None)(event["httpMethod"], event["queryStringParameters"])
   except:
-    pass
-  
+    {'statusCode': 500,
+     'body': None,
+     'headers': {'Content-Type': 'application/json'}}
   return {'statusCode': 200,
           'body': json.dumps(response),
           'headers': {'Content-Type': 'application/json'}}
