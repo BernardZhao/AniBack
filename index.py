dfe6c575c281f881d86b525966e998543d5647b4
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-09-02 17:25:47 -0700

    Added dynamoDB

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 import json
 import datetime
-
+from boto3 import dynamodb 
 
 def handler(event, context):
+    client = boto3.client('dynamodb')
+    response = client.get_item(TableName='Sketches', Key={'topic':{'S':"bernardPenisSuckByManyMen"}})
+
     data = {
-        'event': event,
+        'response': str(response),
         'context': context,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
