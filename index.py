e3dc561d360e45145e808656bae1c0ea5fdc7dd4
Bernard Zhao <bernard.zhao.us@gmail.com>
2018-09-02 18:11:03 -0700

    nut

--- index.py
+++ index.py
@@ -3,14 +3,15 @@ import datetime
 import boto3
 
 def handler(event, context):
-    client = boto3.client('dynamodb')
-    response = client.get_item(TableName='Sketches', Key={'sketchId':{'S':"bernardPenisSuckByManyMen"}})
-    data = {
-        'description': response['Item']['description']['S'],
-        'sketchId': response['Item']['sketchId']['S'],
-        'title': response['Item']['title']['S'],
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+  client = boto3.client('dynamodb')
+  response = client.get_item(TableName='Sketches', Key={'sketchId':{'S':"bernardPenisSuckByManyMen"}})
+  data = {
+    'event': event,
+    'description': response['Item']['description']['S'],
+    'sketchId': response['Item']['sketchId']['S'],
+    'title': response['Item']['title']['S'],
+  }
+  return {'statusCode': 200,
+          'body': json.dumps(data),
+          'headers': {'Content-Type': 'application/json'}}
 
