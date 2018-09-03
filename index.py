ec417b7cbeefbc2b82b813dc02074b2860a64238
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-09-02 18:02:34 -0700

    updated handler to connect to dynamoDB

--- index.py
+++ index.py
@@ -4,13 +4,13 @@ import boto3
 
 def handler(event, context):
     client = boto3.client('dynamodb')
-    response = client.get_item(TableName='Sketches', Key={'topic':{'S':"bernardPenisSuckByManyMen"}})
-
+    response = client.get_item(TableName='Sketches', Key={'sketchId':{'S':"bernardPenisSuckByManyMen"}})
     data = {
-        'event': event,
-        'response': str(response),
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'description': response['Item']['description']['S'],
+        'sketchId': response['Item']['sketchId']['S'],
+        'title': response['Item']['title']['S'],
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
